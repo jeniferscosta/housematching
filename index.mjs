b5cfd686a74af2e10795d409bcdfa38e36842393
Costa <jeniferscosta@gmail.com>
2025-03-15 20:17:17 -0300

    servidor finalmente rodando

--- index.mjs
+++ index.mjs
@@ -1,34 +1,14 @@
-/**
- * @format
- */
-import dotenv from 'dotenv';
-import { AppRegistry } from 'react-native';
-import React from 'react';
-import App from './App.js'; // Ensure App.js uses ESM (with import/export)
-import { connectDB } from './config/db.mjs'; // Ensure db.js uses ESM
-import appConfig from './app.json' assert { type: "json" }; // Needed for JSON imports in ESM
+import http from 'http';
 
-// Load environment variables
-dotenv.config();
-console.log('Environment variables loaded from .env file');
+const hostname = '127.0.0.1';
+const port = 3000;
 
-// Connect to Database
-connectDB();
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
 
-// Register React Native App
-const appName = appConfig.name;
-AppRegistry.registerComponent(appName, () => App);
-
-// Access environment variables
-const dbHost = process.env.DB_HOST;
-const dbUser = process.env.DB_USER;
-const dbPass = process.env.DB_PASS;
-const port = process.env.PORT || 3000;
-const secretKey = process.env.SECRET_KEY;
-
-// Log environment variables (Remove this in production)
-console.log(`Database Host: ${dbHost}`);
-console.log(`Database User: ${dbUser}`);
-console.log(`Database Password: ${dbPass}`);
-console.log(`Server Port: ${port}`);
-console.log(`Secret Key: ${secretKey}`);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
