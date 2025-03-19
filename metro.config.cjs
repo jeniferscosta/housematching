7ba22882a3049164fa119b205036e5551d846e71
Costa <jeniferscosta@gmail.com>
2025-03-19 19:58:57 -0300

    ainda tendo problemas com a emulador do app. estou utilizando expo para emular mas ainda não obtive sucesso e ver a pagina de onboarding abrindo

---
+++ metro.config.cjs
@@ -0,0 +1,11 @@
+// metro.config.js
+const { getDefaultConfig } = require("@expo/metro-config");
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+// SVG Support
+defaultConfig.transformer.babelTransformerPath = require.resolve("react-native-svg-transformer");
+defaultConfig.resolver.assetExts = defaultConfig.resolver.assetExts.filter(ext => ext !== "svg");
+defaultConfig.resolver.sourceExts = [...defaultConfig.resolver.sourceExts, "svg"];
+
+module.exports = defaultConfig;
